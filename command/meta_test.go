5829ee7172bdc43eb38a4914fcdb4ba7604fa3dd
Masayuki Morita <minamijoyo@gmail.com>
2021-06-17 09:48:37 +0900

    Add tests for browse commands

--- command/meta_test.go
+++ command/meta_test.go
@@ -40,3 +40,114 @@ func TestDetectProviderName(t *testing.T) {
 		})
 	}
 }
+
+func TestBuildProviderDocURL(t *testing.T) {
+	cases := []struct {
+		desc string
+		name string
+		want string
+		ok   bool
+	}{
+		{
+			desc: "simple",
+			name: "aws",
+			want: "https://www.terraform.io/docs/providers/aws/index.html",
+			ok:   true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, err := buildProviderDocURL(tc.name)
+			if tc.ok && err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if !tc.ok && err == nil {
+				t.Fatalf("expected an error, but no error: %s", got)
+			}
+
+			if got != tc.want {
+				t.Errorf("got = %s, want = %s", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestBuildResourceDocURL(t *testing.T) {
+	cases := []struct {
+		desc string
+		name string
+		want string
+		ok   bool
+	}{
+		{
+			desc: "simple",
+			name: "aws_security_group",
+			want: "https://www.terraform.io/docs/providers/aws/r/security_group.html",
+			ok:   true,
+		},
+		{
+			desc: "no underscore",
+			name: "foo",
+			want: "",
+			ok:   false,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, err := buildResourceDocURL(tc.name)
+			if tc.ok && err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if !tc.ok && err == nil {
+				t.Fatalf("expected an error, but no error: %s", got)
+			}
+
+			if got != tc.want {
+				t.Errorf("got = %s, want = %s", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestBuildDataDocURL(t *testing.T) {
+	cases := []struct {
+		desc string
+		name string
+		want string
+		ok   bool
+	}{
+		{
+			desc: "simple",
+			name: "aws_security_group",
+			want: "https://www.terraform.io/docs/providers/aws/d/security_group.html",
+			ok:   true,
+		},
+		{
+			desc: "no underscore",
+			name: "foo",
+			want: "",
+			ok:   false,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, err := buildDataDocURL(tc.name)
+			if tc.ok && err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if !tc.ok && err == nil {
+				t.Fatalf("expected an error, but no error: %s", got)
+			}
+
+			if got != tc.want {
+				t.Errorf("got = %s, want = %s", got, tc.want)
+			}
+		})
+	}
+}
