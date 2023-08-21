88c2974d3caa844c607083804f220a10d34e0037
Rui Chen <rui@chenrui.dev>
2023-08-21 13:47:38 -0400

    deps: upgrade to use go1.21 and fix golangci-lint errors
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/minamijoyo/tfschema
 
-go 1.17
+go 1.21
 
 require (
 	github.com/hashicorp/go-hclog v0.15.0
--- tfschema/lock_file.go
+++ tfschema/lock_file.go
@@ -2,7 +2,6 @@ package tfschema
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -18,23 +17,24 @@ import (
 // # This file is maintained automatically by "terraform init".
 // # Manual edits may be lost in future updates.
 //
-// provider "registry.terraform.io/hashicorp/aws" {
-//   version     = "3.17.0"
-//   constraints = "3.17.0"
-//   hashes = [
-//     "h1:BLK4zgpn2O4ojSZhCtAXzqsvm7BymqSxrtcXLp/J/yA=",
-//     "zh:047e22b2e02d57fb1d945d52c4bd062f50a657865b6a8d21f96ba55ef8a474e5",
-//     "zh:30135eb8eed7f4c135c504889cf66a2a586671783122a2953856b7040074cba9",
-//     "zh:557a57801e5c004178caf8c9d94282b17bfab0c6f8fe68e414a4f650b441fc3c",
-//     "zh:6583fbc0159a7835235d7e7b7ce08b5cc51514dc5bc8276c49b3e14a79a7ddea",
-//     "zh:6dc01ed7ea928caf1fd5ace9a25e219b57642925d8784046f994e241adad45d2",
-//     "zh:9c8a1942c21dcb78992a39d2ca54a08d839cc936a68341fb5443983ebd17cad0",
-//     "zh:c71ca4ccb2c228bfb1aa5e127a54872c739fe54c477b4c545df2e74e77ef3bf4",
-//     "zh:c94ed23ed6770721262d7bc342467c02524bb0380ece0122f224e30a6b0d23de",
-//     "zh:e3b71cb43d0602ee1fcf520a7bd7a2a9232b751138e6ef5cf6837caa26c63173",
-//     "zh:f5143912cd4588bc3a19a98a7fd7979202b9c55130c77b2697cf64bfea76c61c",
-//   ]
-// }
+//	provider "registry.terraform.io/hashicorp/aws" {
+//	  version     = "3.17.0"
+//	  constraints = "3.17.0"
+//	  hashes = [
+//	    "h1:BLK4zgpn2O4ojSZhCtAXzqsvm7BymqSxrtcXLp/J/yA=",
+//	    "zh:047e22b2e02d57fb1d945d52c4bd062f50a657865b6a8d21f96ba55ef8a474e5",
+//	    "zh:30135eb8eed7f4c135c504889cf66a2a586671783122a2953856b7040074cba9",
+//	    "zh:557a57801e5c004178caf8c9d94282b17bfab0c6f8fe68e414a4f650b441fc3c",
+//	    "zh:6583fbc0159a7835235d7e7b7ce08b5cc51514dc5bc8276c49b3e14a79a7ddea",
+//	    "zh:6dc01ed7ea928caf1fd5ace9a25e219b57642925d8784046f994e241adad45d2",
+//	    "zh:9c8a1942c21dcb78992a39d2ca54a08d839cc936a68341fb5443983ebd17cad0",
+//	    "zh:c71ca4ccb2c228bfb1aa5e127a54872c739fe54c477b4c545df2e74e77ef3bf4",
+//	    "zh:c94ed23ed6770721262d7bc342467c02524bb0380ece0122f224e30a6b0d23de",
+//	    "zh:e3b71cb43d0602ee1fcf520a7bd7a2a9232b751138e6ef5cf6837caa26c63173",
+//	    "zh:f5143912cd4588bc3a19a98a7fd7979202b9c55130c77b2697cf64bfea76c61c",
+//	  ]
+//	}
+//
 // ```
 //
 // It includes selected the fully qualified provider name and version.
@@ -99,7 +99,7 @@ type Provider struct {
 
 // loadLockFile loads and parses a given lock file.
 func loadLockFile(path string) (*Lock, error) {
-	source, err := ioutil.ReadFile(path)
+	source, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
